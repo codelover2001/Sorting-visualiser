1ec43ca7ee049566d448e4977652c28d3cc57fd9
codelover2001 <63252450+codelover2001@users.noreply.github.com>
2021-06-30 20:39:56 +0530

    Update selection.js

--- selection.js
+++ selection.js
@@ -1,7 +1,7 @@
 let vec = [];
 let bubble = document.querySelector('.bubble');
-let timeout = 500;
-let n = 6;
+let timeout = 400;
+let n = 8;
 let flag = 0;
 function Reset() {
     window.location.reload();
@@ -22,20 +22,30 @@ function GenerateArray() {
         element.setAttribute('class', 'arr');
         element.innerHTML = val;
         NewArray.append(element);
-        console.log(val);
     }
 }
 async function SelectionSortArray() {
-    if (flag > 1)
+    if (flag != 1)
         return;
     flag = 2;
+
     for (let i = 0; i < n; i++) {
+        window.scrollBy(0, 2000);
+
+
+        for (let j = 0; j <= i; j++)
+            document.getElementById((n * i + j + 1)).style.backgroundColor = "rgb(235, 7, 219)";
+        for (let j = i + 1; j < n; j++)
+            document.getElementById((n * i + j + 1)).style.backgroundColor = "rgb(68, 160, 141)";
+
+
+
         let min_idx = i;
+        let ele2 = document.getElementById((n * i) + (min_idx + 1));
+        ele2.style.backgroundColor = "#ffff00";
         for (let j = i + 1; j < n; j++) {
             let ele1 = document.getElementById((n * i) + (j + 1));
-            let ele2 = document.getElementById((n * i) + (min_idx + 1));
             ele1.style.backgroundColor = "red";
-            ele2.style.backgroundColor = "red";
             await new Promise((resolve) =>
                 setTimeout(() => {
                     resolve();
@@ -43,14 +53,18 @@ async function SelectionSortArray() {
             );
             if (vec[j] < vec[min_idx]) {
                 min_idx = j;
+                ele2.style.backgroundColor = "rgb(68, 160, 141)";
+                ele2 = document.getElementById((n * i) + (min_idx + 1));
+                ele2.style.backgroundColor = "#ffff00";
             }
+            else
+                ele1.style.backgroundColor = "rgb(68, 160, 141)";
+
             await new Promise((resolve) =>
                 setTimeout(() => {
                     resolve();
                 }, timeout)
             );
-            ele1.style.backgroundColor = "rgb(68, 160, 141)";
-            ele2.style.backgroundColor = "rgb(68, 160, 141)";
         }
         await new Promise((resolve) =>
             setTimeout(() => {
@@ -58,15 +72,13 @@ async function SelectionSortArray() {
             }, timeout)
         );
         let ele1 = document.getElementById((n * i) + (i + 1));
-        let ele2 = document.getElementById((n * i) + (min_idx + 1));
         let temp = vec[i];
         vec[i] = vec[min_idx];
         vec[min_idx] = temp;
         ele1.innerHTML = vec[i];
         ele2.innerHTML = vec[min_idx];
-        for (let j = 0; j < i; j++)
-            document.getElementById((n * i + j + 1)).style.backgroundColor = "rgb(235, 7, 219)";
-
+        ele2.style.backgroundColor = "rgb(68, 160, 141)";
+        ele1.style.backgroundColor = "rgb(235, 7, 219)";
 
         let NewArray = document.createElement('div');
         NewArray.setAttribute('id', -(i + 1));
